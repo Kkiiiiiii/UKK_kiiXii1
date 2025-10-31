94ac5268f75345a162b5bf17cf6e330fc8af276e
Kkiiiiiii <rikizhakaria@gmail.com>
2025-10-31 12:31:26 +0700

    Add project files.

---
+++ UKK_kiiXii1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace UKK_kiiXii1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            username = new Label();
+            password = new Label();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(307, 126);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Username";
+            textBox1.Size = new Size(273, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(307, 208);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(273, 27);
+            textBox2.TabIndex = 1;
+            textBox2.TabStop = false;
+            // 
+            // username
+            // 
+            username.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            username.AutoSize = true;
+            username.BackColor = Color.Transparent;
+            username.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            username.Location = new Point(307, 90);
+            username.Name = "username";
+            username.Size = new Size(94, 25);
+            username.TabIndex = 2;
+            username.Text = "Username";
+            username.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // password
+            // 
+            password.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            password.AutoSize = true;
+            password.BackColor = Color.Transparent;
+            password.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            password.Location = new Point(307, 166);
+            password.Name = "password";
+            password.Size = new Size(90, 25);
+            password.TabIndex = 3;
+            password.Text = "Password";
+            password.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = Properties.Resources.desola_lanre_ologun_zYgV_NGZtlA_unsplash;
+            pictureBox1.Location = new Point(12, 90);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(264, 176);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Trebuchet MS", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.ImageAlign = ContentAlignment.TopCenter;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(610, 41);
+            label1.TabIndex = 5;
+            label1.Text = "LoginForm";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Font = new Font("Times New Roman", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(307, 256);
+            button1.Name = "button1";
+            button1.Size = new Size(273, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSkyBlue;
+            ClientSize = new Size(608, 323);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(password);
+            Controls.Add(username);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label username;
+        private Label password;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ UKK_kiiXii1/Form1.cs
@@ -0,0 +1,10 @@
+namespace UKK_kiiXii1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
